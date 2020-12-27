49ffa30c66fdbd8897ae6082055c291832c38ae4
sugiyama <fwkym471blue@gmail.com>
2020-12-28 08:32:27 +0900

    document管理画面作成

--- teachhub/admin.py
+++ teachhub/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from teachhub.models import Document
 
-# Register your models here.
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    # 教材一覧で表示するフィールド
+    list_display = ('name', 'file')
+    # 教材一覧で「詳細」へ進むリンク
+    list_display_links = ('name', )
+    # 教材一覧で検索対象のフィールド
+    search_fields = ('name', )
