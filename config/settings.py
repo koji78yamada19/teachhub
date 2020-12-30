1e48487bd6a92f8ddea080c8818b4bbf9eee2433
sugiyama <fwkym471blue@gmail.com>
2020-12-31 07:58:47 +0900

    INSTALLED_APPS カンマ追記

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'teachhub.apps.TeachhubConfig'
+    'teachhub.apps.TeachhubConfig',
 ]
 
 MIDDLEWARE = [
