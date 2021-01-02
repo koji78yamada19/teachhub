62d4cdbc0162856d86ba7530fae64cf6fc03744a
sugiyama <fwkym471blue@gmail.com>
2021-01-02 10:19:50 +0900

    管理画面追加　カテゴリー

--- teachhub/admin.py
+++ teachhub/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from teachhub.models import Document
+from teachhub.models import Document, Category
+
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     # 教材一覧で表示するフィールド
-    list_display = ('name', 'file')
+    list_display = ('name', 'file', )
+    # 教材一覧で「詳細」へ進むリンク
+    list_display_links = ('name', )
+    # 教材一覧で検索対象のフィールド
+    search_fields = ('name', 'category__name')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # 教材一覧で表示するフィールド
+    list_display = ('name', )
     # 教材一覧で「詳細」へ進むリンク
     list_display_links = ('name', )
     # 教材一覧で検索対象のフィールド
