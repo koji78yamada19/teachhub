54cf17ec294de5b36c1678103df097a735858be2
sugiyama <fwkym471blue@gmail.com>
2021-01-02 11:04:28 +0900

    管理画面　資料 ・一覧にカテゴリー表示

--- teachhub/admin.py
+++ teachhub/admin.py
@@ -5,7 +5,7 @@ from teachhub.models import Document, Category
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     # 教材一覧で表示するフィールド
-    list_display = ('name', 'file', )
+    list_display = ('name', 'category', 'file')
     # 教材一覧で「詳細」へ進むリンク
     list_display_links = ('name', )
     # 教材一覧で検索対象のフィールド
