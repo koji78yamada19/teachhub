c7e93398434e35852f44a9c01353c3eb09882a35
sugiyama <fwkym471blue@gmail.com>
2021-01-30 10:19:06 +0900

    フォーム追記　章

--- teachhub/forms.py
+++ teachhub/forms.py
@@ -7,5 +7,5 @@ class DocumentForm(forms.ModelForm):
         # 使用するモデル
         model = Document
         # 使用するフィールド
-        fields = ('name', 'category', 'content', 'file')
+        fields = ('name', 'chapter', 'category', 'content', 'file')
         
