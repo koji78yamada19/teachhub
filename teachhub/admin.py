6e5d723af4479936f0b1630569a59eb0ee515b1c
sugiyama <fwkym471blue@gmail.com>
2021-02-07 10:59:11 +0900

    管理画面に教科書を追加

--- teachhub/admin.py
+++ teachhub/admin.py
@@ -1,21 +1,30 @@
 from django.contrib import admin
-from teachhub.models import Document, Chapter
+from teachhub.models import Document, Chapter, Textbook
 
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     # 教材一覧で表示するフィールド
-    list_display = ('name', 'category', 'chapter','file')
+    list_display = ('name', 'category', 'chapter', 'textbook', 'file')
     # 教材一覧で「詳細」へ進むリンク
     list_display_links = ('name', )
     # 教材一覧で検索対象のフィールド
-    search_fields = ('name', 'category', 'chapter__name')
+    search_fields = ('name', 'category', 'chapter__name', 'textbook__name')
 
 @admin.register(Chapter)
 class ChapterAdmin(admin.ModelAdmin):
     # 教材一覧で表示するフィールド
-    list_display = ('name', )
+    list_display = ('name', 'textbook')
     # 教材一覧で「詳細」へ進むリンク
     list_display_links = ('name', )
     # 教材一覧で検索対象のフィールド
+    search_fields = ('name', 'textbook__name')
+
+@admin.register(Textbook)
+class TextbookAdmin(admin.ModelAdmin):
+
+    list_display = ('name', )
+
+    list_display_links = ('name', )
+
     search_fields = ('name', )
