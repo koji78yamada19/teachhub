01c95ddad4a084e09bd1b4ccdf136364c446ffe7
sugiyama <fwkym471blue@gmail.com>
2021-02-10 09:24:13 +0900

    管理画面追加　節

--- teachhub/admin.py
+++ teachhub/admin.py
@@ -1,20 +1,29 @@
 from django.contrib import admin
-from teachhub.models import Document, Chapter, Textbook
+from teachhub.models import Document, Section, Chapter, Textbook
 
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     # 教材一覧で表示するフィールド
-    list_display = ('name', 'category', 'chapter', 'textbook', 'file')
+    list_display = ('name', 'category', 'section', 'chapter', 'textbook', 'file')
     # 教材一覧で「詳細」へ進むリンク
     list_display_links = ('name', )
     # 教材一覧で検索対象のフィールド
-    search_fields = ('name', 'category', 'chapter__name', 'textbook__name')
+    search_fields = ('name', 'category', 'section__name', 'chapter__name', 'textbook__name')
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'chapter')
+    ordering = ['id',]
+    list_display_links = ('name', )
+
+    search_fields = ('name', 'chapter__name')
 
 @admin.register(Chapter)
 class ChapterAdmin(admin.ModelAdmin):
     # 教材一覧で表示するフィールド
-    list_display = ('textbook','name')
+    list_display = ('name', 'textbook')
     ordering = ['id',]
     # 教材一覧で「詳細」へ進むリンク
     list_display_links = ('name', )
