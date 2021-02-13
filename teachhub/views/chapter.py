959f094e1bd364e345783d838ebfebc430e48afd
sugiyama <fwkym471blue@gmail.com>
2021-02-13 15:57:14 +0900

    章一覧ビュー textbook_id追記

--- teachhub/views/chapter.py
+++ teachhub/views/chapter.py
@@ -5,9 +5,12 @@ from teachhub.models import Chapter, Textbook
 def chapter_list(request, textbook_id):
     chapter = "chapters"
     if request.method == 'GET':
-        chapter_list = Chapter.objects.filter(textbook_id=textbook_id).order_by('id')      
-        chapter = chapter_list.first()
-        text_name = chapter.textbook.name         
+        chapter_list = Chapter.objects.filter(textbook_id=textbook_id).order_by('id')             
+        text = Textbook.objects.get(id=textbook_id)
+        text_name = text.name      
+        # 下記でもnameをとれる
+        # chapter = chapter_list.first()
+        # text_name = chapter.textbook.name
         context = {"chapter_list":chapter_list, "text_name":text_name}
         return render(
             request,
