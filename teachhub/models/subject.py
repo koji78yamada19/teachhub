b33a301c2fe0b28b7aab88079b02f30c5c151a21
koji <koji.yamada1978@gmail.com>
2021-07-10 11:51:11 +0900

    Fix: フィールド名

--- teachhub/models/subject.py
+++ teachhub/models/subject.py
@@ -6,8 +6,8 @@ class Subject(models.Model):
     class Meta:
         db_table = 'subjects'
 
-    subject_name = models.CharField(
+    name = models.CharField(
         verbose_name='科目名', max_length=128, null=False, blank=False)
 
     def __str__(self):
-        return self.subject_name
+        return self.name
