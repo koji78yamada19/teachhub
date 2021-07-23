1678746d350b5b1ca36f59e29b37088d2da071b8
tatsuro-hasegawa <tatsuro11234556@gmial.com>
2021-07-23 14:51:53 +0900

    ファイル名が切れないように修正、アップロード時の挙動修正

--- static/teachhub/js/eleven-page.js
+++ static/teachhub/js/eleven-page.js
@@ -8,3 +8,9 @@ $('.hoge').hover(
     }
 )
 });
+$('input').on('change', function () {
+    var file = $(this).prop('files')[0];
+    $('#upload-file-name').text(file.name);
+    $('.upload-input-label').attr('id', 'upload-input-label');
+    $('#upload-button-submit').removeAttr('id')
+});
