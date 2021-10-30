435eec3e8ade3fce7c66e3bc5aa001870a546a2d
koji <koji.yamada1978@gmail.com>
2021-10-30 15:07:00 +0900

    Fix: settings.pyの修正

--- config/settings/production.py
+++ config/settings/production.py
@@ -40,13 +40,13 @@ DATABASES = {
 
 
 # 静的サーバーの設定（外部パッケージ利用する）
-STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-AZURE_ACCOUNT_NAME = 'teachhubstr'
-AZURE_ACCOUNT_KEY = '7SgcFtXHXjKvDJT/bLVqNzG/qePoW4SUtzsXlFqiW+KKmSp/lXYjVyj4rSaxLCOPmUqe1itw6mOMNw6fXrqb7Q=='
-AZURE_CUSTOM_DOMAIN = 'teachhubstr.blob.core.windows.net'
-AZURE_CONTAINER = 'app'
-STATIC_ROOT = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/static/'
-STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/static/'
-MEDIA_ROOT = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/media/'
-MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/media/'
+# STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
+# AZURE_ACCOUNT_NAME = 'teachhubstr'
+# AZURE_ACCOUNT_KEY = '7SgcFtXHXjKvDJT/bLVqNzG/qePoW4SUtzsXlFqiW+KKmSp/lXYjVyj4rSaxLCOPmUqe1itw6mOMNw6fXrqb7Q=='
+# AZURE_CUSTOM_DOMAIN = 'teachhubstr.blob.core.windows.net'
+# AZURE_CONTAINER = 'app'
+# STATIC_ROOT = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/static/'
+# STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/static/'
+# MEDIA_ROOT = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/media/'
+# MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/media/'
