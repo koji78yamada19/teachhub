1d5543cb1155c9baf832d4c85e72ebe33ae740ae
koji <koji.yamada1978@gmail.com>
2021-11-29 21:51:30 +0900

    Fix: Allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://teahhub.azurewebsites.net', 'localhost']
+# ALLOWED_HOSTS = ['https://teahhub.azurewebsites.net', 'localhost']
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
